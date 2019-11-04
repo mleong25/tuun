6a9cfdee6da821be234c2341dabe199d75707eb5
Marcus Leong <mwleong25@gmail.com>
2019-11-03 22:27:00 -0600

    Initial UI commit'

---
+++ tuunapp-ui/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>T</h1><h1 style={{color:"purple"}}>u</h1><h1>un</h1>
+        <p>
+          A better way to connect with music
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
